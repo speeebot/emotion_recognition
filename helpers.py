634d47d46c5e13ba5279f04173a5b94010af4d38
speeebot <speeebot@gmail.com>
2022-03-31 01:57:25 -0400

    model trains on all data within the dataset. generally above 40% accuracy. diastolic blood pressure seems to be the highest accuracy.

--- helpers.py
+++ helpers.py
@@ -1,5 +1,10 @@
 import argparse, os, sys
-from sklearn.metrics import precision_score, recall_score, confusion_matrix
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+from parse import parse
+from sklearn import preprocessing
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
 
 #-------------------------------input handling--------------------------------
 
@@ -39,5 +44,126 @@ def grab_args():
   args = parser.parse_args()
   return args
 
-  #-------------------------------save/load module------------------------------
+#-------------------------------data handling--------------------------------
 
+def fix_data_type(data_type):
+  if data_type == "EDA":
+    return "EDA"
+  elif data_type == "DIA":
+    return "BP Dia"
+  elif data_type == "volt":
+    return "Resp"
+  elif data_type == "sys":
+    return "LA Systolic BP"
+  elif data_type == "resp":
+    return "Respiration Rate"
+  elif data_type == "pulse":
+    return "Pulse Rate"
+  elif data_type == "mean":
+    return "LA Mean BP"
+  elif data_type == "mmhg":
+    return "BP"
+  else:
+    return "all"
+  
+def normalize_data(data):
+  min_val = np.min(data)
+  max_val = np.max(data)
+  if ((max_val - min_val) > 0):
+    return (data - min_val) / (max_val - min_val)
+  else:
+    return data
+
+def GetData(args, timestep, signal_count):
+  data_type = args.data_type
+  data_path = args.data_directory_path
+  #map user inputs to their file name equivalents
+  data_type = fix_data_type(data_type)
+  #parse file name pattern
+  pattern = '{subject_id}_{class_num}_{file_data_type}_{measurement_unit}.txt'
+
+  #set data path based on training directory or testing directory
+  if args.testOrTrain == "train":
+    validation_data_path = data_path + "/Validation/"
+    data_path += "/Training/"
+  elif args.testOrTrain == "test":
+    data_path += "/Testing/"
+
+  #read training or testing data from directory and save it into an array
+  arr_x = []
+  arr_y = []
+  with os.scandir(data_path) as it:
+    for entry in it:
+      if entry.name.endswith(".txt") and entry.is_file():
+        result = entry.name.split("_")
+        if result[2] == data_type and data_type != "all":
+          #store the class type and data for this iteration of data_type file
+          signals = np.loadtxt(data_path+entry.name)
+          signals = list(signals)
+          #normalize data
+          signals = normalize_data(signals)
+          arr_x.append(signals)
+          label = int(result[1])
+          #decrement labels by one, to fit between 0-9 for the output layer of 10 classes
+          arr_y.append(label-1)
+        elif data_type == "all":
+          #store the class type and data for all data_type files
+          signals = np.loadtxt(data_path+entry.name)
+          signals = list(signals)
+          signals = normalize_data(signals)
+          arr_x.append(signals)
+          label = int(result[1])
+          arr_y.append(label-1)
+
+  #read validation data from directory and save it into an array
+  if args.testOrTrain == "train":
+    arr_valid_x = []
+    arr_valid_y = []
+    with os.scandir(validation_data_path) as it:
+      for entry in it:
+        if entry.name.endswith(".txt") and entry.is_file():
+          result = entry.name.split("_")
+          if result[2] == data_type and data_type != "all":
+            #store the class type and data for this iteration of data_type file
+            signals = np.loadtxt(validation_data_path+entry.name)
+            signals = list(signals)
+            signals = normalize_data(signals)
+            arr_valid_x.append(signals)
+            label = int(result[1])
+            arr_valid_y.append(label-1)
+          elif data_type == "all":
+            #store the class type and data for all data_type files
+            signals = np.loadtxt(validation_data_path+entry.name)
+            signals = list(signals)
+            signals = normalize_data(signals)
+            arr_valid_x.append(signals)
+            label = int(result[1])
+            arr_valid_y.append(label-1)
+
+  #finishing preprocessing data, normalization
+  if args.testOrTrain == "train":
+    #format data
+    x_train = np.array(arr_x, dtype=object)
+    y_train = np.array(arr_y, dtype='float64')
+    x_valid = np.array(arr_valid_x, dtype=object)
+    y_valid = np.array(arr_valid_y, dtype='float64')
+    #pad training data with 0s to make sequences same length
+    x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=timestep, dtype='float32')
+    x_valid = tf.keras.preprocessing.sequence.pad_sequences(x_valid, maxlen=timestep, dtype='float32')
+    #reshape data for model
+    x_train = np.reshape(x_train, (x_train.shape[0], 1, x_train.shape[1]))
+    x_valid = np.reshape(x_valid, (x_valid.shape[0], 1, x_valid.shape[1]))
+
+    return x_train, y_train, x_valid, y_valid
+  elif args.testOrTrain == "test":
+    #format data
+    x_test = np.array(arr_x, dtype=object)
+    y_test = np.array(arr_y, dtype='float64')
+    #pad testing data with 0s to make sequences same length
+    x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=timestep, dtype='float32')
+    #reshape data for model
+    x_test = np.reshape(x_test, (x_test.shape[0], 1, x_test.shape[1]))
+
+    return x_test, y_test
+  else:
+    return x_train, y_train, x_test, y_test
--- project2.py
+++ project2.py
@@ -1,7 +1,3 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
-import numpy as np
-from parse import parse
 from helpers import *
 np.set_printoptions(threshold=sys.maxsize)
 
@@ -9,158 +5,70 @@ np.set_printoptions(threshold=sys.maxsize)
 #1: Happy, 2: Surprised, 3: Sad, 4: Startled/Surprised, 5: Skeptical
 #6: Embarrassed, 7: Scared/Nervous, 8: Physical Pain, 9: Angry, 10: Disgusted
 
-def fix_data_type(data_type):
-  if data_type == "EDA":
-    return "EDA"
-  elif data_type == "DIA":
-    return "BP Dia"
-  elif data_type == "volt":
-    return "Resp"
-  elif data_type == "sys":
-    return "LA Systolic BP"
-  elif data_type == "resp":
-    return "Respiration Rate"
-  elif data_type == "pulse":
-    return "Pulse Rate"
-  elif data_type == "mean":
-    return "LA Mean BP"
-  elif data_type == "mmhg":
-    return "BP"
-  else:
-    return "all"
-
-def normalize_data(data):
-  return (data - np.min(data)) / (np.max(data) - np.min(data))
-
-
-def GetData(args, timestep):
-  data_type = args.data_type
-  data_path = args.data_directory_path
-
-  data_type = fix_data_type(data_type)
-
-  pattern = '{subject_id}_{class_num}_{file_data_type}_{measurement_unit}.txt'
-
-  #set data path based on user input
-  if args.testOrTrain == "train":
-    validation_data_path = data_path + "/Validation/"
-    data_path += "/Training/"
-  elif args.testOrTrain == "test":
-    data_path += "/Testing/"
-
-  file_count = len(os.listdir(data_path))
-
-  #read training or testing data from directory
-  arr = np.empty((0, 2))
-  with os.scandir(data_path) as it:
-    for entry in it:
-      if entry.name.endswith(".txt") and entry.is_file():
-        result = entry.name.split("_")
-        if result[2] == data_type and data_type != "all":
-          signals = np.loadtxt(data_path+entry.name)
-          #store the class type and data for this iteration of data_type file
-          arr = np.append(arr, np.array([[result[1], signals]], dtype=object), axis=0)
-        elif data_type == "all":
-          signals = np.loadtxt(data_path+entry.name)
-          #store the class type and data for all data_type files
-          arr = np.append(arr, np.array([[result[1], signals]], dtype=object), axis=0)
-
-  #read validation data from directory
-  if args.testOrTrain == "train":
-    validation_arr = np.empty((0, 2))
-    with os.scandir(validation_data_path) as it:
-      for entry in it:
-        if entry.name.endswith(".txt") and entry.is_file():
-          result = entry.name.split("_")
-          if result[2] == data_type and data_type != "all":
-            signals = np.loadtxt(validation_data_path+entry.name)
-            #store the class type and data for this iteration of data_type file
-            validation_arr = np.append(validation_arr, np.array([[result[1], signals]], dtype=object), axis=0)
-          elif data_type == "all":
-            signals = np.loadtxt(validation_data_path+entry.name)
-            #store the class type and data for all data_type files
-            validation_arr = np.append(validation_arr, np.array([[result[1], signals]], dtype=object), axis=0)
-
-  if args.testOrTrain == "train":
-    x_train = arr[:, 1]
-    y_train = arr[:, 0]
-    x_test = validation_arr[:, 1]
-    y_test = validation_arr[:, 0] 
-
-    #pad training data with 0 (make sure same length)
-    x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, maxlen=timestep, dtype='float32')
-    x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=timestep, dtype='float32')
-
-    y_train = np.array(y_train, dtype='float32')
-    y_test = np.array(y_test, dtype='float32')
-
-    x_train = normalize_data(x_train)
-    x_test = normalize_data(x_test)
-
-    return x_train, y_train, x_test, y_test
-  elif args.testOrTrain == "test":
-    x_test = arr[:, 1]
-    y_test = arr[:, 0]
-    #pad testing data with 0 (make sure same length)
-    x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=timestep, dtype='float32')
-    y_test = np.array(y_test)   
-
-    return x_test, y_test
-  else:
-    return x_train, y_train, x_test, y_test
-
-def MakeModel(numFeatures, timestep, signal_count):
+def MakeModel(num_features, timestep, signal_count):
   #create sequential model
   model = tf.keras.models.Sequential()
-  #create Embedding layer - takes input and makes it easy to use with LSTM
-  #can "manually" do this yourself if you don't want to use embedding
-  model.add(tf.keras.layers.Embedding(signal_count, numFeatures, input_length=timestep))
-  #uncomment link below for vanilla RNN
-  #model.add(tf.keras.layers.SimpleRNN(64))
-  #uncomment line below for bidirectional LSTM
-  #model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)))
-  #uncomment line below to add another LSTM layer - return_sequences=True passes all hidden states to the next layer which is needed. Setting this to true is needed for all 
-  #model.add(tf.keras.layers.LSTM(64, return_sequences=True))
-  model.add(tf.keras.layers.LSTM(64))
-  model.add(tf.keras.layers.Dense(128))
-  model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-  model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 
+  model.add(tf.keras.layers.LSTM(64, input_shape=(1, 1000), activation='relu', return_sequences=True))
+  model.add(tf.keras.layers.Dropout(0.2))
+  model.add(tf.keras.layers.LSTM(32, activation='sigmoid'))
+  model.add(tf.keras.layers.Dropout(0.2))
+  model.add(tf.keras.layers.Dense(10, activation='sigmoid'))
+  model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
   model.summary()
-  
   return model
 
 def Predict(model, x_test, y_test):
   #predict and format output to use with sklearn
   predict = model.predict(x_test)
   predict = np.argmax(predict, axis=1)
-  #macro precision and recall
-  precisionMacro = precision_score(y_test, predict, average='macro')
-  recallMacro = recall_score(y_test, predict, average='macro')
-  #micro precision and recall
-  precisionMicro = precision_score(y_test, predict, average='micro')
-  recallMicro = recall_score(y_test, predict, average='micro')
+  #macro precision, recall, and F1 score
+  precision_macro = precision_score(y_test, predict, average='macro')
+  recall_macro = recall_score(y_test, predict, average='macro')
+  f1_macro = f1_score(y_test, predict, average='macro')
+  #micro precision, recall, and F1 score
+  precision_micro = precision_score(y_test, predict, average='micro')
+  recall_micro = recall_score(y_test, predict, average='micro')
+  f1_micro = f1_score(y_test, predict, average='micro')
+  #confusion matrix
   confMat = confusion_matrix(y_test, predict)
 
-  print("Macro precision: ", precisionMacro)
-  print("Micro precision: ", precisionMicro)
-  print("Macro recall: ", recallMacro)
-  print("Micro recall: ", recallMicro)
+  print("Macro precision: ", precision_macro)
+  print("Micro precision: ", precision_micro)
+  print("Macro recall: ", recall_macro)
+  print("Micro recall: ", recall_micro)
+  print("Macro F1 score: ", f1_macro)
+  print("Micro F1 score: ", f1_micro)
   print(confMat)
 
-def Train(args, numFeatures, timestep, signal_count):
-  x_train, y_train, x_test, y_test = GetData(args, timestep)
-  model = MakeModel(numFeatures, timestep, signal_count)
+def Train(args, num_features, timestep, signal_count, num_epochs):
+  print("Loading Training Data")
+  #get training data
+  x_train, y_train, x_valid, y_valid = GetData(args, timestep, signal_count)
+
+  print('Fx_train shape:', x_train.shape)
+  print('Fy_train shape:', y_train.shape)
+  print('Fx_valid shape:', x_valid.shape)
+  print('Fy_valid shape:', y_valid.shape)
+  print('Fx_train type:', x_train.dtype)
+  print('Fy_train type:', y_train.dtype)
+  print('Fx_valid type:', x_valid.dtype)
+  print('Fy_valid type:', y_valid.dtype)
+
+  #make the model
+  model = MakeModel(num_features, timestep, signal_count)
   #train model
-  model.fit(np.array(x_train), np.array(y_train), 
-            batch_size=numFeatures, 
-            epochs=5,
-            validation_data=[x_test, y_test])
+  model.fit(x_train, y_train, 
+            batch_size=num_features, 
+            epochs=num_epochs,
+            validation_data=(x_valid, y_valid),
+            shuffle=True)
+  #save model
   model.save("./models/"+args.model_name+".h5")
   print("Model saved.")
 
-def Test(args, timestep):
+def Test(args, timestep, signal_count):
   print("Loading Test Data")
-  x_test, y_test = GetData(args)
+  x_test, y_test = GetData(args, timestep, signal_count)
   print("Loading model")
   model = tf.keras.models.load_model("./models/"+args.model_name+".h5")
   print("Making predictions on test data")
@@ -170,14 +78,15 @@ def main():
   #grab and validate the user arguments
   args = grab_args()
 
-  numFeatures = 128
-  timestep = 200
-  signal_count = 1001
+  num_epochs = 50
+  num_features = 64
+  timestep = 1000
+  signal_count = 1000
 
   if args.testOrTrain == "train":
-    Train(args, numFeatures, timestep, signal_count)
+    Train(args, num_features, timestep, signal_count, num_epochs)
   elif args.testOrTrain == "test":
-    Test(args, timestep)
+    Test(args, timestep, signal_count)
 
 if __name__ == "__main__":
   main()
